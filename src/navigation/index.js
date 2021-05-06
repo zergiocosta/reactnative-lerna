aee549df3e34e10a42bdd9866710539ca4899c06
Sergio Costa <sergio.costa@outlook.com>
2021-05-06 13:50:05 -0300

    fix: main nav update

--- src/navigation/index.js
+++ src/navigation/index.js
@@ -1,18 +1,18 @@
-import React from 'react';
-import { NavigationContainer } from '@react-navigation/native';
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import React from 'react'
+import { NavigationContainer } from '@react-navigation/native'
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs'
 import AppOne from '../app1'
 import AppTwo from '../app2'
 
-const Tab = createBottomTabNavigator();
+const Tab = createBottomTabNavigator()
 
-export default function Navigation() {
-  return (
-    <NavigationContainer>
-      <Tab.Navigator>
-        <Tab.Screen name="AppOne" component={AppOne} />
-        <Tab.Screen name="AppTwo" component={AppTwo} />
-      </Tab.Navigator>
-    </NavigationContainer>
-  );
-}
+const Navigation = () => (
+  <NavigationContainer>
+    <Tab.Navigator>
+      <Tab.Screen name="AppOne" component={AppOne} />
+      <Tab.Screen name="AppTwo" component={AppTwo} />
+    </Tab.Navigator>
+  </NavigationContainer>
+)
+
+export default Navigation
